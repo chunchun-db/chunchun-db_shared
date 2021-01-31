243f1dec5facb334cd2433804278897bf9a373be
Aliaksei Labkovich <alexei.labkovich@gmail.com>
2021-01-31 20:58:17 +0300

    Can add items with id

--- src/interfaces/ICollection.ts
+++ src/interfaces/ICollection.ts
@@ -4,7 +4,7 @@ export interface ICollection<T extends IRecord> {
     name: string;
 
     getAll(): Promise<T[]>;
-    add(items: Array<Omit<T, 'id'>>): Promise<void>;
+    add(items: Array<Omit<T, 'id'> & Partial<Pick<T, 'id'>>>): Promise<void>;
     remove(items: Array<T['id']>): Promise<void>;
     update(item: T): Promise<void>;
 }
